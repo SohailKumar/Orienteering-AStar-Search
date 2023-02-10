003a60b2259f7f5947611ef7b39107dbe577b6f4
SohailK <sohail.kumar03@gmail.com>
2023-02-10 14:26:13 -0500

    Added terrain type data weight.

--- lab1.py
+++ lab1.py
@@ -1,3 +1,5 @@
+#TODOS
+
 from math import sqrt
 from PIL import Image, ImageDraw
 import sys
@@ -5,6 +7,8 @@ import sys
 # openL = [[0]*395 for i in range(500)]
 # closedL = [[0]*395 for i in range(500)]
 elevationL = [[0]*395 for i in range(500)]
+# terrainL = [[0]*395 for i in range(500)]
+imgT = ""
 
 class Node:
     x = -1
@@ -17,15 +21,41 @@ class Node:
     def __init__(self, x, y, goalX, goalY, g, parent):
         self.x = x
         self.y = y
-        self.w = elevationL[y][x]                                           #elevation value
+        try:
+            self.w = elevationL[y][x]   
+        except:
+            print("Y: ", y-1, "X", x-1)                                     #elevation value
         self.h = sqrt((goalX - x)**2 + (goalY - y)**2 + (elevationL[goalY][goalX] - self.w)**2)     #3D Euclidean Distance heuristic value
         self.g = g                                                          #distance from goal
         self.parent = parent
-        self.f = self.g + self.w * self.h                                                   
+        self.f = self.g + self.w * self.h * getValue(self.x,self.y)                                               
         
     def __str__(self):
         return f'(x: {self.x}, y: {self.y})\n\tw: {self.w}, h(n): {self.h}, g(n): {self.g}, parent: {self.parent}'
 
+
+def getValue(x,y):
+    color = imgT.getpixel((x,y))
+    if color == (205,0,101):    #out of bounds
+        return 9999999999
+    elif color == (0,0,0):      #footpath
+        return 1
+    elif color == (71,51,3):    #paved road
+        return 2
+    elif color == (0,0,255):    #lake/swamp/marsh
+        return 20
+    elif color == (5,73,24):    #impassible vegeation
+        return 10
+    elif color == (2,136,40):   #walk forest
+        return 7
+    elif color == (2,208,60):   #slow run forest
+        return 5
+    elif color == (255,255,255):#easy movement forest
+        return 4
+    elif color == (255,192,0):  #rough meadow
+        return 7
+    elif color == (248,148,18): #open land
+        return 3
 
 def SetupElevationList(elevationFile):
     with open(elevationFile) as elev:
@@ -64,13 +94,14 @@ def Path(node):
 
 def GetNeighbors(node, end):
     neighbors = []
-    if(node.x >= 0):
+
+    if(node.x >= 0 and node.x <= 393):
         neighbors.append(Node(node.x+1, node.y, end[0], end[1], node.g+10.29, node))
-    if(node.x <= 394):
+    if(node.x <= 394 and node.x >= 1):
         neighbors.append(Node(node.x-1, node.y, end[0], end[1], node.g+10.29, node))
-    if(node.y >= 0):
+    if(node.y >= 0 and node.y <= 498):
         neighbors.append(Node(node.x, node.y+1, end[0], end[1], node.g+7.55, node))
-    if(node.y <= 499):
+    if(node.y <= 499 and node.y >= 1):
         neighbors.append(Node(node.x, node.y-1, end[0], end[1], node.g+7.55, node))
     return neighbors
 
@@ -94,14 +125,6 @@ def AStarSetup(start, end):
         for i in neighs:
             if i.x == end[0] and i.y == end[1]:
                 return Path(i)
-            # if openL.setdefault((i.x,i.y)) is not None:
-            #     print("TYPE1: ", type(openL[(i.x,i.y)].f))
-            #     print("TYE2:", i.f)
-            #     print("TYPE3: ", type(closedL[(i.x,i.y)]))
-            #     if openL[(i.x,i.y)].f > i.f or closedL[(i.x,i.y)].f > i.f:
-            #         openL[(i.x,i.y)] = i
-            # else:
-            #     openL[(i.x,i.y)] = i
             if (openL.get((i.x,i.y)) is not None and openL[(i.x,i.y)].f < i.f) or (closedL.get((i.x,i.y)) is not None and closedL[(i.x,i.y)].f < i.f):
                 continue
             else:
@@ -123,7 +146,7 @@ if __name__ == "__main__":
 
     terrainImage = "testcases/stripElevation/terrain.png"
     elevationFile = "testcases/stripElevation/mpp.txt"
-    pathFile = "testcases/stripElevations/path.txt"
+    pathFile = "testcases/stripElevation/path.txt"
     outputImageFileName = "output.png"
 
     SetupElevationList(elevationFile)
@@ -133,8 +156,9 @@ if __name__ == "__main__":
         for line in f.readlines():
             destinations.append([int(m) for m in line.split(" ")])
 
-    img = Image.open(terrainImage)
-    imgNew = img.copy()
+    imgT = Image.open(terrainImage)
+    print(imgT.getpixel((0,0)))
+    imgNew = imgT.copy()
     # print("PATH: ", AStarSetup((0,0),(1,1)))
     for i in range(len(destinations)-1):
         print("Destination", i, destinations[i], "Destination", i+1, destinations[i+1])
