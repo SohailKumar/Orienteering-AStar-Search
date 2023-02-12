dab7d4d6bdad071bcbf5b86b974599d8eecf759d
SohailK <sohail.kumar03@gmail.com>
2023-02-12 17:27:25 -0500

    Minor changes(names and rearranging) + added documentation. Updated README.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Orienteering-AStar-Search
- 
+
+
+This program calculates the most optimal path through a given set of points for
+a given image describing terrain taking into account the elevation of the land
+(also passed into the program).
+
+It uses a Weighted A* Search to find this path:
+COST: 
+    The cost of going from one pixel to the next is calculated using the
+    following formula:
+    cost = distance from parent pixel * terrain weight value
+    eg. cost from footpath to a walk-forest pixel to the right would be:
+    
+    The total cost of a pixel is calculated as follows:
+    totalCost = parent cost + distance from parent pixel * terrain weight 
+
+    The terrain weights are given as follows:
+    Out of Bounds = 9999999999
+    Footpath = 1
+    Paved Road = 2
+    Lake/Swamp/Marsh = 20
+    Impassible Vegeation = 10
+    Walk Forest = 7
+    Slow Run Forest = 5
+    Easy Movement Forest = 4
+    Rough Meadow = 6
+    Open Land = 3
+    
+
+HEURISTIC:
+    The heuristic function used by the Weighted A* Search is the 3D Euclidean
+    Distance from the pixel to the goal:
+    pixel heuristic = 
+        sqrt(   (goalX - currX)**2 + (goalY - currY)**2 + 
+                (elevation of goal pixel - elevation of current pixel)**2   )
+    
+    This heuristic is *admissible* because the most ideal path from the current
+    pixel to the goal pixel is a straight path. There can be no path shorter
+    than one that goes straight from the current pixel to the goal. All paths
+    will either be the same or be longer since there are elevation changes which
+    inherently increase path length or there are terrain changes which make
+    certain paths more inefficient due to the "cost" of using them.
+
+    This heuristic is also *consistent* because the cost of going from one pixel
+    to another plus the cost of going from that pixel to the goal will always be
+    less than or equal to the cost of going from the first pixel to the goal.
+    That is just inherent in the nature of the formula. 
+
--- lab1.py
+++ lab1.py
@@ -1,22 +1,25 @@
-#TODOS
+# AUTHOR: Sohail "Sol" Kumar
 
 from math import sqrt
 import random
 from PIL import Image, ImageDraw
 import sys
 
-elevationL = [[0]*395 for i in range(500)]
-imgT = ""
+elevationL = [[0]*395 for i in range(500)] #2D array to store elevation values
+imgT = "" #Stores terrain image to get terrain values
 
+# Describes the state of a single pixel
 class Node:
-    x = -1
-    y = -1
-    w = -1
-    h = -1
-    g = -1
-    parent = -1
-    f = -1
-    distance = -1
+    x = -1          #x position of pixel
+    y = -1          #y position of pixel
+    w = -1          #weight of pixel(terrain value)
+    h = -1          #heuristic of pixel(3D euclidean distance to goal)
+    g = -1          #cost of going to pixel from parent(g = parent.distance) + 3D euclidean distance from parent to pixel
+    parent = -1     #parent of pixel
+    f = -1          #estimated cost of pixel
+    distance = -1   #distance from start to pixel for the current path
+
+    #constructor for the node/pixel
     def __init__(self, x, y, goalX, goalY, distanceTravelled, parent):
         self.x = x
         self.y = y
@@ -32,11 +35,14 @@ class Node:
             self.distance = parent.distance + sqrt(distanceTravelled[0]**2 + distanceTravelled[1]**2 + (elevationL[y][x] - elevationL[parent.y][parent.x])**2)
         self.parent = parent
         self.f = self.g + self.w * self.h                                             
-        
+
+    #toString for the node/pixel 
     def __str__(self):
         return f'(x: {self.x}, y: {self.y})\n\tw: {self.w}, h(n): {self.h}, g(n): {self.g}, parent: {self.parent}'
 
 
+# Returns the terrain value of the pixel at the given coordinates using the terrain
+# given to the program using the color of the pixel.
 def getValue(x,y):
     color = imgT.getpixel((x,y))
     if color == (205,0,101):    #out of bounds
@@ -63,7 +69,7 @@ def getValue(x,y):
         print(color)
         sys.exit
 
-
+# Populates a 2D array with the elevations of each pixel coordinate.
 def SetupElevationList(elevationFile):
     with open(elevationFile) as elev:
         y = 0
@@ -78,14 +84,15 @@ def SetupElevationList(elevationFile):
                     break
             y+=1
 
-
+# Draws a red path between coordinates given an array of pixels.
 def DrawPath(path, imgNew):
     drawNew = ImageDraw.Draw(imgNew)
     i = 0
     for i in range(len(path)-1):
         drawNew.line([(path[i][0], path[i][1]),(path[i+1][0], path[i+1][1])], fill='red', width=1)
     
-
+# Calculates the final path of the search by looping through the parents of each
+# node/pixel until it reaches the starting node/pixel.
 def Path(node):
     totPath = []
     totPath.append((node.x, node.y))
@@ -94,7 +101,7 @@ def Path(node):
         node = node.parent
     return totPath
 
-
+# Gets the neighbors for a given node/pixel and creates them using the Node constructor.
 def GetNeighbors(node, end):
     neighbors = []
 
@@ -108,33 +115,38 @@ def GetNeighbors(node, end):
         neighbors.append(Node(node.x, node.y-1, end[0], end[1], (0,7.55), node))
     return neighbors
 
-def AStarSetup(start, end):
+# Runs the A star algorithm to find a path from the starting node/pixel to the goal node/pixel.
+def AStar(start, end):
     openL = {}
     closedL = {}
+    
+    #add starting node to open list:
     openL[(start[0],start[1])] = Node(start[0], start[1], end[0], end[1], (0,0), None)
+    
     while len(openL)>0:
         min = sys.maxsize
         minJ = -1
         
-        for j in openL: #find smallest f
+        for j in openL: #find smallest f in open list
             if openL[j].f < min:
                 min = openL[j].f
                 minJ = j
 
-        
         n = openL[minJ]
         del openL[minJ] #remove node from dictionary
 
         neighs = GetNeighbors(n, end)
+
+        closedL[n.x, n.y] = n
+
         for i in neighs:
             if i.x == end[0] and i.y == end[1]:
                 return Path(i), i.distance
             
+            #if in the closed list, don't consider it.  
             if closedL.get((i.x,i.y)) is not None:
                 continue
 
-            closedL[(i.x,i.y)] = i
-            
             if (openL.get((i.x,i.y)) is None or openL[(i.x,i.y)].g > i.g):
                 openL[(i.x,i.y)] = i
     return [], -1
@@ -148,6 +160,7 @@ if __name__ == "__main__":
 
     SetupElevationList(elevationFile)
 
+    # Get array of all destinations to go to:
     destinations = []
     with open(pathFile) as f:
         for line in f.readlines():
@@ -158,11 +171,11 @@ if __name__ == "__main__":
     imgNew = img.copy()
     totDistance = 0
     for i in range(len(destinations)-1):
-        path, distance = AStarSetup(destinations[i], destinations[i+1])
+        path, distance = AStar(destinations[i], destinations[i+1])
         totDistance += distance
         DrawPath(path,imgNew)
     print("Total Distance:", totDistance, "m")
-    imgNew.show()
+
     imgNew.save(outputImageFileName)
 
 
